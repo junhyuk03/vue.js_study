690e9b8fb8f120b20cc98ce37273a33789203f27
sku101-junhyuk <junhyuk@skuniv.ac.kr>
2022-12-19 17:05:25 +0900

    # 2022-12-19
    - 노드버드 강의 공부
     - eslint 설정
     - 회원가입 기능
     - 회원가입 더미 데이터 생성

--- ch1/front/pages/signup.vue
+++ ch1/front/pages/signup.vue
@@ -3,35 +3,40 @@
     <v-container>
       <v-card>
         <v-container>
-            <v-form v-model = 'valid'>
+            <v-form ref="form" v-model = 'valid' @submit.prevent="onSubmitForm">
               <v-subheader>회원가입</v-subheader>
               <v-text-field
                 v-model = 'email'
                 label = '이메일'
                 type = 'email'
+                :rules="emailRules"
                 required
               />
               <v-text-field
-                v-model = 'passward'
+                v-model = 'password'
                 label = '비밀번호'
-                type = 'passward'
+                type = 'password'
+                :rules="passwordRules"
                 required
               />
               <v-text-field
-                v-model = 'passwardCheck'
+                v-model = 'passwordCheck'
                 label = '비밀번호 확인'
-                type = 'passward'
+                type = 'password'
+                :rules="passwordCheckRules"
                 required
               />
               <v-text-field
                 v-model = 'nickname'
                 label = '닉네임'
                 type = 'nickname'
+                :rules="nicknameRules"
                 required
               />
               <v-checkbox
                 v-model = 'terms'
                 required
+                :rules="[v => !!v || '약관에 동의해야 합니다']"
                 label = '개인정보 동의'
               />
               <v-btn color = 'green' type="submit"> 가입하기 </v-btn>
@@ -45,17 +50,40 @@
 <script>
     export default {
         data() {
-            return{
+            return {
               valid: false,
               email: '',
-              passward: '',
-              passwardCheck: '',
+              password: '',
+              passwordCheck: '',
               nickname: '',
               terms: false,
-              test: '',
+              emailRules: [
+                v => !!v || '이메일은 필수입니다',
+                v=> /.+@.+/.test(v) || '이메일이 유효하지 않습니다',
+              ],
+              nicknameRules: [
+                v => !!v || '닉네임은 필수입니다',
+              ],
+              passwordRules: [
+                v => !!v || '비밀번호는 필수입니다',
+              ],
+              passwordCheckRules: [
+                v => !!v || '비밀번호 확인은 필수입니다',
+                v => v == this.password || '비밀번호가 일치하지 않습니다'
+              ],
             }
         },
-    }
+      methods : {
+          onSubmitForm() {
+            if(this.$refs.form.validate()) {
+              this.$store.dispatch('user/signUp', {
+                nickname: this.nickname,
+                email: this.email,
+              })
+            }
+          },
+      },
+    };
 </script>
 
 <style>
